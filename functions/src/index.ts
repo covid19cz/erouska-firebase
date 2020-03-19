3e60917143eb9674618b7eb3f1fbef12732eb2a6
Jakub Beránek <berykubik@gmail.com>
2020-03-19 09:28:29 +0100

    Add timestamp to table and change BUID generation

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,20 +4,27 @@ import {isLeft} from "fp-ts/lib/Either";
 import * as admin from 'firebase-admin';
 import {CollectionReference} from "@google-cloud/firestore";
 import {Guid} from "guid-typescript";
+import {PathReporter} from "io-ts/lib/PathReporter";
 
 const Request = t.type({
     platform: t.string,
     platformVersion: t.string,
     manufacturer: t.string,
     model: t.string,
-    local: t.string,
+    locale: t.string,
 });
 
-const MAX_GUID_RETRIES = 10;
+const MAX_BUID_RETRIES = 10;
+const BUID_LENGTH = 10;
+
+// Initialize Firebase
+admin.initializeApp({
+    credential: admin.credential.applicationDefault()
+});
 
 async function generateGuid(collection: CollectionReference) {
-    for (let i = 0; i < MAX_GUID_RETRIES; i++) {
-        const buid = Guid.create().toString().substring(14).replace("-", "");
+    for (let i = 0; i < MAX_BUID_RETRIES; i++) {
+        const buid = Guid.create().toString().substring(14).replace(/-/g, "").substr(0, BUID_LENGTH);
         const res = await collection.where("buid", "==", buid).get();
 
         // Note: small data race here, ignored for now
@@ -28,39 +35,52 @@ async function generateGuid(collection: CollectionReference) {
     return null;
 }
 
+function getUTCTimestamp(): number {
+    return Math.floor(Date.now() / 1000);
+}
+
 export const createUser = functions.region('europe-west2').https.onCall(async (data, context) => {
     if (!context.auth) {
         throw new functions.https.HttpsError('failed-precondition', 'Missing authentication');
     }
-    const payload = Request.decode(data);
-    if (isLeft(payload)) {
+    const parsed = t.exact(Request).decode(data);
+    if (isLeft(parsed)) {
+        console.error(PathReporter.report(parsed));
         throw new functions.https.HttpsError('invalid-argument', 'Wrong arguments');
     }
-
-    admin.initializeApp({
-        credential: admin.credential.applicationDefault()
-    });
-
+    const payload = parsed.right;
     const uid = context.auth.uid;
-    const auth: { phone_number: string } = context.auth as {} as { phone_number: string };
 
-    const collection = admin.firestore().collection("users");
-    const buid = generateGuid(collection);
-    if (buid === null) {
-        throw new functions.https.HttpsError('unavailable', 'BUID could not be generated');
+    const phoneNumber = context.auth.token.phone_number;
+    if (phoneNumber === undefined) {
+        throw new functions.https.HttpsError('failed-precondition', 'Phone number is missing');
     }
 
+    const collection = admin.firestore().collection("users");
+
     const document = collection.doc(uid);
-    try {
+    const snapshot = await document.get();
+
+    let buid: string | null;
+    if (snapshot.exists) {
+        buid = snapshot.get("buid");
+
+        console.log(`Updating user ${uid}`);
+        await document.update({...payload});
+    } else {
+        buid = await generateGuid(collection);
+        if (buid === null) {
+            throw new functions.https.HttpsError('unavailable', 'BUID could not be generated');
+        }
+
+        // Note: small data race here, will throw if already exists
         await document.create({
             ...payload,
             buid,
-            phoneNumber: auth.phone_number,
-            infected: false
+            phoneNumber,
+            infected: false,
+            createdAt: getUTCTimestamp()
         });
-    } catch (e) {
-        await document.set({...payload});
-        console.error(e);
     }
     return {buid};
 });
