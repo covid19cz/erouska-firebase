0acb39ed393283c15d92baa53b970de76fa175a3
Jakub Beránek <berykubik@gmail.com>
2020-03-27 18:51:14 +0100

    Delete old backups before running database backup

--- functions/src/functions/database-backup.ts
+++ functions/src/functions/database-backup.ts
@@ -1,16 +1,26 @@
 import * as functions from "firebase-functions";
 import {BUCKET_URL, REGION} from "../settings";
 import * as firestore from "@google-cloud/firestore";
+import {storage} from "firebase-admin";
 
 async function backupDatabase() {
-    const client = new firestore.v1.FirestoreAdminClient();
-    const bucket = `gs://${BUCKET_URL}/backups/database`;
+    const prefix = "backups/database/";
 
+    // delete old backups
+    const bucket = storage().bucket(BUCKET_URL);
+    await bucket.deleteFiles({
+        prefix,
+        force: true
+    });
+
+    const client = new firestore.v1.FirestoreAdminClient();
+    const timestamp = Date.now().toString();
+    const outputUriPrefix = `gs://${BUCKET_URL}/${prefix}${timestamp}`;
     const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT;
     const databaseName = client.databasePath(projectId, "(default)");
-    return await client.exportDocuments({
+    await client.exportDocuments({
         name: databaseName,
-        outputUriPrefix: bucket,
+        outputUriPrefix,
         collectionIds: []
     });
 }
