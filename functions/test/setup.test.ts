ff22d47edb6c941e8abe1fc8438a1538ce7d90fb
Jakub Beránek <berykubik@gmail.com>
2020-04-01 21:42:00 +0200

    CI: make tests work on CI

--- functions/test/setup.test.ts
+++ functions/test/setup.test.ts
@@ -1,26 +1,30 @@
+/* tslint:disable no-implicit-dependencies */
 import {spawn} from "child_process";
 import * as fftest from "firebase-functions-test";
+import {waitUntilUsed} from "tcp-port-used";
+import {clearFirestoreData} from "@firebase/testing";
 
 // initialize admin
 const PROJECT_ID = "erouska-test";
+const PORT = 8080;
 process.env.GCLOUD_PROJECT = PROJECT_ID;
-process.env.FIRESTORE_EMULATOR_HOST = "localhost:8080";
+process.env.FIRESTORE_EMULATOR_HOST = `localhost:${PORT}`;
 // @ts-ignore
 const _ = require("../src/index");
-
-import {clearFirestoreData} from "@firebase/testing";
 
 export const FIREBASE_TEST = fftest();
 
 const emulator = spawn("firebase", ["emulators:start", "--only", "firestore"], {cwd: process.cwd()});
 
 // wait for emulator to start
-setTimeout(() => {
-    run();
-}, 3000);
+waitUntilUsed(PORT, 500, 30000)
+    .then(run, () => {
+        console.log("Timed out waiting for Firestore emulator to start");
+        process.exit(1);
+    });
 
 beforeEach(async () => {
-  await clearFirestoreData({ projectId: PROJECT_ID });
+    await clearFirestoreData({projectId: PROJECT_ID});
 });
 
 after(() => {
