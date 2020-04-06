7125052ab5e119a04cbc309fe2d9344dcb0d86a6
Roman Pichlik <dagi@gooddata.com>
2020-04-06 22:03:38 +0200

    Sligthly improved logging

--- functions/src/triggered/delete-old-users.ts
+++ functions/src/triggered/delete-old-users.ts
@@ -7,12 +7,13 @@ const MAX_USER_LIFETIME_MS = 1000 * 3600 * 24 * 30 * 6;
 
 async function deleteOldUsers() {
     let pageToken: string | undefined = undefined;
+    const cutDay = new Date(new Date().getTime() - MAX_USER_LIFETIME_MS);
+    console.log(`Users and all their assets created before ${cutDay} will be deleted accordingly to the service terms & conditions`);
     while (true) {
         const users: ListUsersResult = await FIREBASE_AUTH_CLIENT.listUsers(1000, pageToken);
         for (const user of users.users) {
             const creationDate = new Date(user.metadata.creationTime);
-            const expiryDate = new Date(creationDate.getTime() + MAX_USER_LIFETIME_MS);
-            if (expiryDate < new Date()) {
+            if (creationDate < cutDay) {
                 try {
                     console.log(`Deleting user ${user.uid} created at ${creationDate}`);
                     await FIREBASE_AUTH_CLIENT.deleteUser(user.uid);
